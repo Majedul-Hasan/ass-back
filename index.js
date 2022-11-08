d5bf71cdee3e850dcab5e659832b79aa8e2b21e7
farhan sharif <farhansharif1410@gmail.com>
2022-11-08 20:56:51 +0530

    create database in mongodb

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ app.get('/', (req, res) => {
     res.send('My server is Ready For Running')
 })
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.PROJECT_NAME}:${process.env.PROJECT_PASSWORD}@cluster0.dfmvdpa.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 app.listen(port, (req, res) => {
     console.log('Server is Running on port in ', port);
 })
